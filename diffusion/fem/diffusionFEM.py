e8b73b2af359afe3158751ff43aed062846be668
christian_pedersen21@hotmail.com <christian_pedersen21@hotmail.com>
2017-06-02 21:05:20 +0200

    added wave

--- diffusion/fem/diffusionFEM.py
+++ diffusion/fem/diffusionFEM.py
@@ -1,20 +1,31 @@
 from dolfin import *
+from mshr import *
+
+#mesh = Mesh('mesh/pipe.xml')
 
 
-mesh = Mesh('mesh/pipe.xml')
+mesh = UnitSquareMesh(20, 20)
 
 x0 = 0
 y0 = 0
 ro = 1.0
 ri = 0.5
-T = 2
-dt = 0.1
-t = 0
+end_time = 120.0
+dt = 0.2
+t = 0.0
+"""
+rec = Rectangle(Point(0.0, 0.0), Point(5.0,5.0))
+cir1 = Circle(Point(2.0, 1.0), 0.2)
+cir2 = Circle(Point(2.0, 3.8), 0.6)
 
+domain = rec -cir1 - cir2
+
+mesh = generate_mesh(domain, 50)
+"""
 V = FunctionSpace(mesh, 'CG', 1)
 
-K = Expression('pow(x[0]-x0, 2) + pow(x[1]-y0, 2) - pow(ri, 2) > DOLFIN_EPS ? 0.01 : 2', x0=x0, y0=y0, ri=ri)
-k = interpolate(K, V)
+K = Expression('pow(x[0]-x0, 2) + pow(x[1]-y0, 2) - pow(ri, 2) > DOLFIN_EPS ? 0.0001 : 0.002', x0=x0, y0=y0, ri=ri)
+k = Constant(0.2)#interpolate(K, V)
 
 T = TrialFunction(V)
 Tt = TestFunction(V)
@@ -29,27 +40,56 @@ class Inner(SubDomain):
     def inside(self, x ,on_boundary):
         return (pow(x[0]-x0, 2) + pow(x[1]-y0, 2) - pow(ri, 2) < DOLFIN_EPS) and on_boundary
 
+class Left(SubDomain):
+    def inside(self, x, on_boundary):
+        return x[0] < DOLFIN_EPS and on_boundary
+
+class Right(SubDomain):
+    def inside(self, x, on_boundary):
+        return x[0] > 1 - DOLFIN_EPS and on_boundary
+
+class Top(SubDomain):
+    def inside(self, x, on_boundary):
+        return x[1] > 1 - DOLFIN_EPS and on_boundary
 
 mf = FacetFunction('size_t', mesh)
 mf.set_all(0)
-outer = Outer()
-outer.mark(mf, 1)
-inner = Inner()
-inner.mark(mf, 2)
+#outer = Outer()
+#outer.mark(mf, 1)
+#inner = Inner()
+#inner.mark(mf, 2)
+left = Left()
+left.mark(mf, 2)
+right = Right()
+right.mark(mf, 3)
+top = Top()
+top.mark(mf, 4)
+plot(mf, interactive=True)
 
+n = FacetNormal(mesh)
 
-IC = Expression('x[0]*x[1]')
+BC = DirichletBC(V, Constant(2), left)
+BC2 = DirichletBC(V, Constant(5), right)
+BC3 = DirichletBC(V, Constant(3), top)
+
+bcs = [BC2, BC, BC3]
+
+IC = Expression('x[0] < 0.5 ? 2 : 5')#DirichletBC(V, Constant(5.0), mf, 2)
 T0 = interpolate(IC, V)
+#IC.apply(T0)
 
-eq = Constant(1./dt)*dot((T-T0), Tt)*dx + k*dot(grad(T), grad(Tt))*dx# - k*inner(grad(T), Tt)*ds
+eq = Constant(1./dt)*(T-T0)*Tt*dx + k*dot(grad(T), grad(Tt))*dx# - k*(dot(grad(T), n))*Tt*ds
+#A1 = assemble(lhs(eq))
+#IC.apply(A1)
+T1 = Function(V)
 
-T = Function(V)
+#plot(IC, interactive=True)
 
-while t < T+DOLFIN_EPS:
+while t < end_time+DOLFIN_EPS:
     print t
     t += dt
-    solve(lhs(eq) == rhs(eq), T)
-    T.assign(T0)
-    plot(T, rescale=False)
+    solve(lhs(eq) == rhs(eq), T1, bcs)
+    T0.assign(T1)
+    plot(T0, rescale=False)
     
 
---
+++ wave/lswFD.py
@@ -0,0 +1,136 @@
+from numpy import linspace, zeros, newaxis
+
+def solver(I, V, f, q, Lx, Ly, Nx, Ny, dt, T, b,
+           user_action = None, dt_safety_factor = 1, 
+           boundary_y0 = 'reflecting', boundary_yM = 'reflecting', 
+           boundary_x0 = 'reflecting', boundary_xM = 'reflecting'):
+
+    Nt = int(round(T/float(dt)))
+    t = linspace(0, T*dt, Nt+1)      # mesh points in time    
+    x = linspace(0, Lx, Nx+1)        # mesh points in x dir
+    y = linspace(0, Ly, Ny+1)        # mesh points in y dir
+
+    dx = x[1] - x[0]
+    dy = y[1] - y[0]
+
+    xv = x[:,newaxis]
+    yv = y[newaxis,:]
+    
+    It = range(0, Nt)
+    
+    #Check q
+    if q is None or q == 0:
+        print 'q is not a valid function or value'
+        return
+    
+    #Pre-calculate the functions for scalar
+    Q = zeros((Nx+3, Ny+3))
+    Q[1:-1,1:-1] = q(xv, yv)
+    q = Q
+    q[1:-1, 0]  = q[1:-1, 2]
+    q[1:-1, -1] = q[1:-1, -3]
+    q[0, 1:-1]  = q[2, 1:-1]
+    q[-1, 1:-1] = q[-3, 1:-1]
+
+    #V = V(xv, yv)
+
+
+    #Set constants used in loops
+    Cx = dt / dx
+    Cy = dt / dy
+    K  = dt*b / 2
+    dt2 = dt**2
+    
+    u = zeros((Nx+3, Ny+3))     # solution array
+    u_1 = zeros((Nx+3, Ny+3))   # solution at t-dt
+    u_2 = zeros((Nx+3, Ny+3))   # solution at t-2*dt
+    
+    #Calculate initial conditions into u_1  
+    u_1[1:-1,1:-1] = I(xv[:], yv[:])
+    
+    #Update ghost points for initial conditions
+    u_1[-1,1:-1] = u_1[-3,1:-1]
+    u_1[1:-1,0] = u_1[1:-1,2]
+    u_1[1:-1,-1] = u_1[1:-1,-3]
+    u_1[0,1:-1] = u_1[2,1:-1]
+    
+    if user_action is not None:
+        user_action(u_1, x, xv, y, yv, t, 0)
+        
+    #Calculate first time step
+#    for i in Ix:
+#        for j in Iy:
+#            u[i,j] = Cx**2/2*(                                                           \
+#                            (0.5*(q[i+1,j] + q[i,j]) * (u_1[i+1,j] - u_1[i,j]) -        \
+#                             0.5*(q[i,j] + q[i-1,j]) * (u_1[i,j] - u_1[i-1,j])))        \
+#                   + Cy**2/2*(                                                           \
+#                            (0.5*(q[i,j+1] + q[i,j]) * (u_1[i,j+1] - u_1[i,j]) -        \
+#                            (0.5*(q[i,j] + q[i,j-1]) * (u_1[i,j] - u_1[i,j-1]))))       \
+#                   + 0.5*dt2*f(x[i], y[j], t[1]) + u_1[i,j] + V(x[i], y[j])*dt*(1+K)
+                   
+    u[1:-1, 1:-1] = \
+               Cx**2/2*(                                                   \
+                (0.5*(q[2:,1:-1] + q[1:-1,1:-1]) * (u_1[2:,1:-1] - u_1[1:-1,1:-1]) - \
+                 0.5*(q[1:-1,1:-1] + q[:-2,1:-1]) * (u_1[1:-1,1:-1] - u_1[:-2,1:-1])))        \
+             + Cy**2/2*(                                                           \
+                (0.5*(q[1:-1,2:] + q[1:-1,1:-1]) * (u_1[1:-1,2:] - u_1[1:-1,1:-1]) - \
+                 0.5*(q[1:-1,1:-1] + q[1:-1,:-2]) * (u_1[1:-1,1:-1] - u_1[1:-1,:-2])))       \
+             + 0.5*dt2*f(xv[:], yv[:], t[1]) + u_1[1:-1,1:-1] + V(xv[:], yv[:])*dt*(1+K)
+
+    #Update ghost points
+    u[1:-1, 0]  = u[1:-1, 2]
+    u[1:-1, -1] = u[1:-1, -3]
+    u[0, 1:-1]  = u[2, 1:-1]
+    u[-1, 1:-1] = u[-3, 1:-1]  
+
+    if user_action is not None:
+        user_action(u, x, xv, y, yv, t, 1)
+    
+    # Update data structures for next step
+    u_2, u_1, u = u_1, u, u_2     
+    #Recursive calculations
+    for n in It:
+#        for i in Ix:
+#            for j in Iy:
+#                u[i,j] = (Cx**2*(                                                        \
+#                        (0.5*(q[i+1,j] + q[i,j]) * (u_1[i+1,j] - u_1[i,j]) -            \
+#                         0.5*(q[i,j] + q[i-1,j]) * (u_1[i,j] - u_1[i-1,j])))              \
+#                     + Cy**2*(                                                           \
+#                        (0.5*(q[i,j+1] + q[i,j]) * (u_1[i,j+1] - u_1[i,j]) -            \
+#                        (0.5*(q[i,j] + q[i,j-1]) * (u_1[i,j] - u_1[i,j-1]))))           \
+#                     + dt2*f(x[i], y[j], t[n]) + 2*u_1[i,j] - u_2[i,j]*(1-K))/(1+K)
+        u[1:-1,1:-1] = \
+                 (Cx**2*( \
+                    (0.5*(q[2:,1:-1] + q[1:-1,1:-1]) * (u_1[2:,1:-1] - u_1[1:-1,1:-1]) -    \
+                     0.5*(q[1:-1,1:-1] + q[:-2,1:-1]) * (u_1[1:-1,1:-1] - u_1[:-2,1:-1])))  \
+                + Cy**2*(                                                                   \
+                    (0.5*(q[1:-1,2:] + q[1:-1,1:-1]) * (u_1[1:-1,2:] - u_1[1:-1,1:-1]) -    \
+                     0.5*(q[1:-1,1:-1] + q[1:-1,:-2]) * (u_1[1:-1,1:-1] - u_1[1:-1,:-2])))  \
+                + dt2*f(xv[:], yv[:], t[n]) + 2*u_1[1:-1,1:-1] - u_2[1:-1,1:-1]*(1-K))/(1+K)
+
+        
+        #Update ghost points
+        u[1:-1, 0]  = u[1:-1, 2]
+        u[1:-1, -1] = u[1:-1, -3]
+        u[0, 1:-1]  = u[2, 1:-1]
+        u[-1, 1:-1] = u[-3, 1:-1] 
+        
+        if user_action is not None:
+            user_action(u, x, xv, y, yv, t, n+1)
+        
+        # Update data structures for next step
+        u_2, u_1, u = u_1, u, u_2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
