2a8797ee528ca4672cf145c081030a4557ea6084
Christian Pedersen <Christian@1x-193-157-201-66.uio.no>
2017-10-28 13:53:40 +0200

    changed from 1d to 2d

--- advection/advectionFD.py
+++ advection/advectionFD.py
@@ -1,48 +1,88 @@
 import numpy as np
-import matplotlib
-matplotlib.use('TkAgg')
-import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import pyplot, cm
 
-Nx = 40
-x0 = 0
-xR = 2
+Nx, Ny = 50, 50
+x0, y0 = 0, 0
+x1, y1 = 1, 1
+dx = float(x1 - x0) / Nx
+dy = float(y1 - y0) / Ny
+
+x = np.linspace(x0, x1, Nx+1)
+y = np.linspace(y0, y1, Ny+1)
+
+
+if dx <= dy:
+    dt = dy**2
+else:
+    dt = dx**2
+
 t0 = 0
-ts = 200
-lmbda = 0.5
-a0 = 0.1
-
-x = np.linspace(x0, xR, Nx+1)
-dx = x[1] - x[0]
-dt = 0.5*dx
-Nt = (ts-t0) / dt
+ts = 5
+Nt = float(ts) / dt
+t = np.linspace(t0, ts, Nt+1)
 
 C = float(dt)/dx
 
-t = np.linspace(t0, ts, Nt+1)
-u0 = np.zeros(Nx+1)
-u1 = np.zeros(Nx+1)
 
+u0 = np.zeros([Nx+1, Ny+1])
+u1 = np.zeros([Nx+1, Ny+1])
+v0 = np.zeros([Ny+1, Nx+1])
+v1 = np.zeros([Ny+1, Nx+1])
 
-I = lambda x: np.sin(x)
+def initial(X, Y):
+    topx, topy = 0.6*x1, 0.6*y1
+    bottomx, bottomy = 0.4*x1, 0.4*y1
+    if bottomx <= X <= topx and bottomy <= Y <= topy:
+        return 10
+    else:
+        return 2
 
-u0[:] = I(x[:])
-print u0
-plt.ion()
+for i in range(len(x)):
+    for j in range(len(y)):
+        u0[i, j] = initial(x[i], y[j])
+        v0[j, i] = initial(y[j], x[i])
+#u0[:, :] = initial(x[:], y[:])
+print u0, v0
+#fig = pyplot.figure(figsize=(11, 7), dpi=100)
+
+#fig = pyplot.ion()
 #u1[0] = u0[0] + C*(u0[1]-u0[0])
 #u1[-1] = u0[-1] - C*(u0[-2]-u0[-1])
-u1[0] = I(dt)
+#u1[0] = I(dt)
 #u1[-1] = u0[-1] - C*(u0[-1]-u0[-2])
-
+c=1
 for n in range(len(t)):
     #u1[0] = u0[0] + C*(u0[1]-u0[0])
-    for i in range(1, len(x)-1):
-        u1[i] = ( u0[i] + 0.25*C*u0[i]*u1[i-1] ) / \
-                    ( 1 + 0.25*C*(u0[i+1] - u0[i-1]) + 0.25*C*u0[i] )
+    u1[1:, 1:] = (u0[1:, 1:] -
+                 (u0[1:, 1:] * c * dt / dx * (u0[1:, 1:] - u0[1:, :-1])) -
+                  v0[1:, 1:] * c * dt / dy * (u0[1:, 1:] - u0[:-1, 1:]))
+    v1[1:, 1:] = (v0[1:, 1:] -
+                 (u0[1:, 1:] * c * dt / dx * (v0[1:, 1:] - v0[1:, :-1])) -
+                 v0[1:, 1:] * c * dt / dy * (v0[1:, 1:] - v0[:-1, 1:]))
+
+    u1[0, :] = 0
+    u1[-1, :] = 0
+    u1[:, 0] = 0
+    u1[:, -1] = 0
+
+    v1[0, :] = 0
+    v1[-1, :] = 0
+    v1[:, 0] = 0
+    v1[:, -1] = 0
+
     u0 = u1
-    u1[0] = u1[1] + C*(u0[1]-u0[0])
-    u1[-1] = u0[-1] - C*(u0[-1]-u0[-2])
-    plt.plot(x, u1)
-    plt.draw()
-    plt.pause(.005)
-    plt.clf()
+    v0 = v1
+    # plt.plot(x, u1)
+    # plt.draw()
+    # plt.pause(.005)
+    # plt.clf()
     #plt.show()
+fig = pyplot.figure(figsize=(11, 7), dpi=100)
+ax = fig.gca(projection='3d')
+X, Y = np.meshgrid(x, y)
+
+ax.plot_surface(X, Y, u1, cmap=cm.jet, rstride=2, cstride=2)
+ax.set_xlabel('$x$')
+ax.set_ylabel('$y$');
+pyplot.show()
